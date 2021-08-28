f53e5b015fd0764b9a1e55bf78afeff0a6b76d13
chris <cseiter@gmail.com>
2021-08-28 18:38:45 -0400

    bird model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_28_223815) do
+
+  create_table "birds", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
