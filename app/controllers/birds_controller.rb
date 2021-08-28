537b56732a4e17f31254b4d68f7429a956656c92
chris <cseiter@gmail.com>
2021-08-28 19:30:45 -0400

    show controller completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,5 +4,14 @@ class BirdsController < ApplicationController
         render json: Bird.all
     end
 
-    
+    def show
+        bird = Bird.find_by(id: params[:id])
+        #if bird ? (render json: bird) : (render json: { error: "Bird not found" }, status: :not_found)
+        if bird
+            render json: bird
+        else
+            render json: { error: "Bird not found" }, status: :not_found
+        end
+
+    end
 end
